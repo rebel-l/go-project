a15041d547625cbfe778fbb4c470d7aa47d0815d
Lars Gaubisch <l.gaubisch@googlemail.com>
2019-04-28 23:48:48 +0200

    added colors and professional prompt example

--- go.mod
+++ go.mod
@@ -1,3 +1,12 @@
 module github.com/rebel-l/go-project
 
 go 1.12
+
+require (
+	github.com/c-bata/go-prompt v0.2.3
+	github.com/fatih/color v1.7.0
+	github.com/mattn/go-colorable v0.1.1 // indirect
+	github.com/mattn/go-runewidth v0.0.4 // indirect
+	github.com/mattn/go-tty v0.0.0-20190424173100-523744f04859 // indirect
+	github.com/pkg/term v0.0.0-20190109203006-aa71e9d9e942 // indirect
+)
--- main.go
+++ main.go
@@ -3,16 +3,34 @@ package main
 import (
 	"fmt"
 
+	"github.com/c-bata/go-prompt"
+	"github.com/fatih/color"
+
 	"github.com/rebel-l/go-project/workingdir"
 )
 
 func main() {
 	fmt.Println()
-	fmt.Println("Welcome to Go-Project Tool ...")
+
+	title := color.New(color.Bold, color.FgGreen)
+	_, _ = title.Println("Welcome to Go-Project Tool ...")
 
 	if err := workingdir.Init(); err != nil {
 		fmt.Printf("Init working directory failed: %s", err)
 	}
 
 	fmt.Printf("The current directory is %s", workingdir.Get())
+
+	fmt.Println("Please select table.")
+	t := prompt.Input("> ", completer)
+	fmt.Println("You selected " + t)
+}
+
+func completer(d prompt.Document) []prompt.Suggest {
+	s := []prompt.Suggest{
+		{Text: "users", Description: "Store the username and age"},
+		{Text: "articles", Description: "Store the article text posted by user"},
+		{Text: "comments", Description: "Store the text commented to articles"},
+	}
+	return prompt.FilterHasPrefix(s, d.GetWordBeforeCursor(), true)
 }
