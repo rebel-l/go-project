e2630229ae00ea4b0081e9366632bbb7fee5a43f
Lars Gaubisch <l.gaubisch@googlemail.com>
2020-06-08 22:26:25 +0200

    optimized sql generator

--- model/generator_sql.go
+++ model/generator_sql.go
@@ -20,13 +20,13 @@ func (s *sql) Generate(m *model) error {
 	destPath := path.Join(s.rootPath, "scripts", "sql")
 	fileName := fmt.Sprintf("%s_create_%ss.sql", time.Now().Format("20060102"), strings.ToLower(m.Name))
 
-	pattern := filepath.Join("./model/tmpl", "*.tmpl")
+	tmplName := filepath.Join(".", "model", "tmpl", "sql.tmpl")
 	funcMap := template.FuncMap{
 		"ToLower":     strings.ToLower,
 		"SqliteField": SqliteField,
 	}
 
-	tmpl, err := template.New("sql").Funcs(funcMap).ParseGlob(pattern)
+	tmpl, err := template.New("sql").Funcs(funcMap).ParseFiles(tmplName)
 	if err != nil {
 		return fmt.Errorf("failed to load templates: %s", err)
 	}
